75df28e5e1a03616349cec47ad95bae8ba87dfc5
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2021-03-23 17:49:29 -0600

    Add schema for paragraphs

--- drupal/modules/custom/manati_graphql/src/Plugin/GraphQL/Schema/ManatiSchema.php
+++ drupal/modules/custom/manati_graphql/src/Plugin/GraphQL/Schema/ManatiSchema.php
@@ -3,12 +3,14 @@
 namespace Drupal\manati_graphql\Plugin\GraphQL\Schema;
 
 use Drupal\node\NodeInterface;
+use Drupal\paragraphs\ParagraphInterface;
 use Drupal\graphql\GraphQL\ResolverBuilder;
 use Drupal\graphql\GraphQL\ResolverRegistry;
 use Drupal\graphql\Plugin\GraphQL\Schema\SdlSchemaPluginBase;
 use Drupal\manati_graphql\Wrappers\QueryConnection;
 use Drupal\block_content\BlockContentInterface;
 use Drupal\media\MediaInterface;
+use Drupal\Core\Url;
 use GraphQL\Error\Error;
 
 /**
@@ -41,6 +43,10 @@ class ManatiSchema extends SdlSchemaPluginBase {
     $this->addMediaImageFields($registry, $builder);
     $this->addMediaFileFields($registry, $builder);
     $this->addMediaRemoteVideoFields($registry, $builder);
+    $this->addParagraphInterfaceTypeResolver($registry, $builder);
+    $this->addParagraphCtaFields($registry, $builder);
+    $this->addParagraphFileFields($registry, $builder);
+    $this->addParagraphLinkFields($registry, $builder);
 
     // Re-usable connection type fields.
     $this->addConnectionFields('LandingPageConnection', $registry, $builder);
@@ -96,6 +102,13 @@ class ManatiSchema extends SdlSchemaPluginBase {
       $builder->produce('entity_load')
         ->map('type', $builder->fromValue('media'))
         ->map('bundles', $builder->fromValue(['remote_video']))
+        ->map('id', $builder->fromArgument('id'))
+    );
+
+    $registry->addFieldResolver('Query', 'paragraphLink',
+      $builder->produce('entity_load')
+        ->map('type', $builder->fromValue('paragraph'))
+        ->map('bundles', $builder->fromValue(['link']))
         ->map('id', $builder->fromArgument('id'))
     );
   }
@@ -340,13 +353,28 @@ class ManatiSchema extends SdlSchemaPluginBase {
     );
 
     $registry->addFieldResolver('Card', 'field_image',
-      $builder->fromPath('entity:node', 'field_image.0.entity')
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:node'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_image.entity'))
     );
 
     $registry->addFieldResolver('Card', 'body',
       $builder->produce('block_formatted_text')
         ->map('entity', $builder->fromParent())
         ->map('field', $builder->fromValue('body'))
+    );
+
+    $registry->addFieldResolver('Card', 'field_link',
+      $builder->produce('entity_reference_revisions')
+        ->map('entity', $builder->fromParent())
+        ->map('field', $builder->fromValue('field_link'))
+    );
+
+    $registry->addFieldResolver('Card', 'field_cta',
+      $builder->produce('entity_reference_revisions')
+        ->map('entity', $builder->fromParent())
+        ->map('field', $builder->fromValue('field_cta'))
     );
   }
 
@@ -403,20 +431,32 @@ class ManatiSchema extends SdlSchemaPluginBase {
     );
 
     $registry->addFieldResolver('MediaImage', 'alt',
-      $builder->fromPath('entity:media', 'field_media_image.0.alt')
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:media'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_media_image.alt'))
     );
 
     $registry->addFieldResolver('MediaImage', 'width',
-      $builder->fromPath('entity:media', 'field_media_image.0.width')
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:media'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_media_image.width'))
     );
 
     $registry->addFieldResolver('MediaImage', 'height',
-      $builder->fromPath('entity:media', 'field_media_image.0.height')
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:media'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_media_image.height'))
     );
 
     $registry->addFieldResolver('MediaImage', 'imageURL',
       $builder->compose(
-        $builder->fromPath('entity:media', 'field_media_image.0.entity'),
+        $builder->produce('property_path')
+          ->map('type', $builder->fromValue('entity:media'))
+          ->map('value', $builder->fromParent())
+          ->map('path', $builder->fromValue('field_media_image.entity')),
         $builder->produce('image_url')
           ->map('entity', $builder->fromParent())
       )
@@ -439,14 +479,20 @@ class ManatiSchema extends SdlSchemaPluginBase {
 
     $registry->addFieldResolver('MediaFile', 'fileURL',
       $builder->compose(
-        $builder->fromPath('entity:media', 'field_media_file.0.entity'),
+        $builder->produce('property_path')
+          ->map('type', $builder->fromValue('entity:media'))
+          ->map('value', $builder->fromParent())
+          ->map('path', $builder->fromValue('field_media_file.entity')),
         $builder->produce('image_url')
           ->map('entity', $builder->fromParent())
       )
     );
 
     $registry->addFieldResolver('MediaFile', 'description',
-      $builder->fromPath('entity:media', 'field_media_file.0.description')
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:media'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_media_file.description'))
     );
   }
 
@@ -465,8 +511,95 @@ class ManatiSchema extends SdlSchemaPluginBase {
     );
 
     $registry->addFieldResolver('MediaRemoteVideo', 'videoURL',
-      $builder->fromPath('entity:media', 'field_media_oembed_video.0.value')
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:media'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_media_oembed_video.value'))
+    );
+  }
+
+  /**
+   * Undocumented function.
+   */
+  protected function addParagraphInterfaceTypeResolver(ResolverRegistry $registry, ResolverBuilder $builder) {
+    // Tell GraphQL how to resolve the ParagraphInterface interface.
+    $registry->addTypeResolver('ParagraphInterface', function ($entity) {
+      if ($entity instanceof ParagraphInterface) {
+        switch ($entity->bundle()) {
+          case 'cta':
+            return 'ParagraphCta';
+          case 'file':
+            return 'ParagraphFile';
+          case 'link':
+            return 'ParagraphLink';
+        }
+
+      }
+      throw new Error('Could not resolve paragraph type.');
+    });
+  }
+
+  /**
+   * Undocumented function.
+   */
+  protected function addParagraphLinkFields(ResolverRegistry $registry, ResolverBuilder $builder) {
+    $registry->addFieldResolver('ParagraphLink', 'id',
+      $builder->produce('entity_id')
+        ->map('entity', $builder->fromParent())
     );
 
+    $registry->addFieldResolver('ParagraphLink', 'linkURL',
+      $builder->compose(
+        $builder->produce('property_path')
+          ->map('path', $builder->fromValue('field_link.uri'))
+          ->map('value', $builder->fromParent())
+          ->map('type', $builder->fromValue('entity:paragraph')),
+        $builder->callback(function($uri) {
+          return Url::fromUri($uri)->toString();
+        })
+      )
+    );
+
+    $registry->addFieldResolver('ParagraphLink', 'linkTitle',
+      $builder->produce('property_path')
+        ->map('path', $builder->fromValue('field_link.title'))
+        ->map('value', $builder->fromParent())
+        ->map('type', $builder->fromValue('entity:paragraph'))
+    );
+  }
+
+  /**
+   * Undocumented function.
+   */
+  protected function addParagraphCtaFields(ResolverRegistry $registry, ResolverBuilder $builder) {
+    $registry->addFieldResolver('ParagraphCta', 'id',
+      $builder->produce('entity_id')
+        ->map('entity', $builder->fromParent())
+    );
+
+
+    $registry->addFieldResolver('ParagraphCta', 'file_link',
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:paragraph'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_file_link.entity'))
+    );
+  }
+
+  /**
+   * Undocumented function.
+   */
+  protected function addParagraphFileFields(ResolverRegistry $registry, ResolverBuilder $builder) {
+    $registry->addFieldResolver('ParagraphFile', 'id',
+      $builder->produce('entity_id')
+        ->map('entity', $builder->fromParent())
+    );
+
+    $registry->addFieldResolver('ParagraphFile', 'file',
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:paragraph'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_file.entity'))
+  );
   }
 }
